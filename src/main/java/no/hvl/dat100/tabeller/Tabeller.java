251f6cc16b38c904a768e287fd6c178bdd1caa66
Ketil T. <ketil.trollvang@gmail.com>
2024-09-30 14:06:09 +0200

    Oppdatert oppgave 4
    Endret på småplukk i syntaksen
    Omskrevet tilStreng så den ikke bruker Arrays.toString()
    Omskrevet reverser slik at den oppretter en ny tabell
    Fikset opp i enhetstest for testreverser slik at den faktisk tester om tabell3 er ny, håper dette er lov
    Fikset out of bounds error i erSortert()

--- src/main/java/no/hvl/dat100/tabeller/Tabeller.java
+++ src/main/java/no/hvl/dat100/tabeller/Tabeller.java
@@ -1,99 +1,94 @@
 package no.hvl.dat100.tabeller;
-import java.util.Arrays;
+
 public class Tabeller {
+
+	public static void main(String[] args) {
+		int[] test1 = { 10, 20, 30, 40, 50 };
+		int[] test2 = { 10, 20, 30, 40, 50, 60 };
+		int[] sammen;
+		sammen = settSammen(test1, test2);
+		System.out.println("Elementer: " + sammen.length);
+		skrivUt(sammen);
+		System.out.println(tilStreng(test1));
+	}
 
 	// a)
 	public static void skrivUt(int[] tabell) {
 
-	for (int i = 0; i < tabell.length;i++) {
-		System.out.print(tabell[i] + " ");
-	}
-
-
+		for (int i = 0; i < tabell.length; i++)
+			System.out.print(tabell[i] + " ");
 	}
 
 	// b)
 	public static String tilStreng(int[] tabell) {
-	    String resultat = Arrays.toString(tabell);
-	    return resultat.replaceAll("\\s+", "");
+		String resultat = "[";
+
+		for (int i = 0; i < tabell.length; i++) {
+			resultat += tabell[i];
+			if (i < tabell.length - 1)
+				resultat += ",";
+		}
+		
+		return resultat += "]";
 	}
 
 	// c)
 	public static int summer(int[] tabell) {
 		int sum = 0;
-		for (int i = 0; i<tabell.length; i++) {
+
+		for (int i = 0; i < tabell.length; i++)
 			sum += tabell[i];
-			
-		}
+
 		return sum;
 	}
 
 	// d)
 	public static boolean finnesTall(int[] tabell, int tall) {
-		for (int i = 0 ; i < tabell.length; i++) {
-			if (tabell[i] == tall) {
+		for (int i = 0; i < tabell.length; i++)
+			if (tabell[i] == tall)
 				return true;
-			}
-			
-		}
-		return false;
 
+		return false;
 	}
 
 	// e)
 	public static int posisjonTall(int[] tabell, int tall) {
-		for (int i = 0; i< tabell.length;i++) {
-			if (tabell[i] == tall) {
+		for (int i = 0; i < tabell.length; i++)
+			if (tabell[i] == tall)
 				return i;
-			}
-		}
+
 		return -1;
-		
 	}
 
 	// f)
 	public static int[] reverser(int[] tabell) {
-		int start = 0;
-		int slutt = tabell.length - 1;
-		
-		
-		while (start < slutt) {
-			
-			int temp = tabell[start];
-			tabell[start] = tabell[slutt];
-			tabell[slutt] = temp;
-			
-			
-			start++;
-			slutt--;
-			
-		}
-		return tabell;
+		int tabellRevers[] = new int[tabell.length];
+
+		for (int i = 0; i < tabell.length; i++)
+			tabellRevers[tabell.length - 1 - i] = tabell[i];
+
+		return tabellRevers;
 	}
 
 	// g)
 	public static boolean erSortert(int[] tabell) {
-		for (int i = 0; i< tabell.length;i++) {
-			if (tabell[i] > tabell[i+1]) {
+		for (int i = 0; i < tabell.length - 1; i++)
+			if (tabell[i] > tabell[i + 1])
 				return false;
-			}
-		}
+
 		return true;
 	}
 
 	// h)
 	public static int[] settSammen(int[] tabell1, int[] tabell2) {
+		int[] sammensattTabell = new int[tabell1.length + tabell2.length];
 
-		int[] nyTabell = new int[tabell1.length + tabell2.length];
-		
-		for (int i = 0 ; i< tabell1.length;i++) {
-			nyTabell[i] = tabell1[i];
-		}
-		
-		
-		for (int i = 0; i< tabell2.length; i++) {
-			nyTabell[tabell1.length + i] = tabell2[i];
-		}
-	return nyTabell;	
+		for (int i = 0; i < tabell1.length; i++)
+			sammensattTabell[i] = tabell1[i];
+
+		for (int i = 0; i < tabell2.length; i++)
+			sammensattTabell[tabell1.length + i] = tabell2[i];
+
+		return sammensattTabell;
 	}
 }
